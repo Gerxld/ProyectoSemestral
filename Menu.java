83275473dcacefd4c6a251b54ac28db66af9d0c5
Simon Espino <sespinoc131005@gmail.com>
2023-11-15 17:58:06 -0500

    Estructura basica de menu

--- Menu.java
+++ Menu.java
@@ -1,3 +1,16 @@
-public static void main(String[] args) {
-    
+// Integrantes: Simon Espino, Gerald Ríos, Javier Reyes y Jirak Anria
+
+import java.io.*;
+
+public class Menu {
+    public static void main(String[] args) throws IOException  {
+        try {
+            // Declaraciones de variables
+
+            // Proceso
+
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error");
+        }
+    }
 }
