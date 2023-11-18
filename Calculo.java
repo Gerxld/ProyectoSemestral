6eda52e325ed05086972738370935cf38c3f5d2a
Javier-05 <150872389+Javier-05@users.noreply.github.com>
2023-11-18 17:01:38 -0500

    Update Calculo.java

--- Calculo.java
+++ Calculo.java
@@ -4,5 +4,11 @@ public class Calculo {
         return tablero;
     }
 
-
+//Este método limpia la consola cada ve que sea llamado (Sólo funciona para Windows).
+    public static void limpiarPantalla() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls")
+        .inheritIO()
+        .start()
+        .waitFor();
+    }
 }
